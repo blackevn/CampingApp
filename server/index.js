54430484c5dfdbebb8ce0c9f11a01edb576983a2
blackevn <kevinlouie55@gmail.com>
2023-03-26 17:36:35 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import bodyParser from "body-parser"
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/users.js"
+
+
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+app.use(cors())
+
+
+app.use("/posts", postRoutes)
+app.use("/user", userRoutes)
+
+
+
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+.then(() => app.listen(PORT, () => console.log(`Server is running: ${PORT}`)))
+
+    .catch(error => console.log(error.message))
+
